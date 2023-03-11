76aca10c046cc97894899c4799ff6ff44cf17629
EagerProgrammer <yth960617@unist.ac.kr>
2023-03-11 15:30:09 +0900

    reupdate : error 해결 필요

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,4 +1,17 @@
-import { Injectable } from "@nestjs/common";
+import { AuthService } from './auth.service';
+import { Injectable } from '@nestjs/common';
+import { Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class LocalStrategy extends
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authservice: AuthService) {
+    super({
+      usernameField: 'email',
+    });
+  }
+  async validate(email: string, password: string): Promise<User> {
+    return this.authservice.getAuthenticatedUser(email, password);
+  }
+}
